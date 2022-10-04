99a466ab4606e050fe5657982e6c786b051aa9e8
Damian Plewa <damianp96@gmail.com>
2022-10-04 08:17:54 +0200

    feat(decorators): added more http methods
    fixes: #3

--- projects/ngx-rest/src/lib/decorators/methods.ts
+++ projects/ngx-rest/src/lib/decorators/methods.ts
@@ -18,4 +18,11 @@ const MethodDecorator = (method: Methods) => (path?: string, options?: any) => {
 };
 
 export const Get = MethodDecorator(Methods.GET);
+export const Head = MethodDecorator(Methods.HEAD);
 export const Post = MethodDecorator(Methods.POST);
+export const Put = MethodDecorator(Methods.PUT);
+export const Delete = MethodDecorator(Methods.DELETE);
+export const Connect = MethodDecorator(Methods.CONNECT);
+export const Options = MethodDecorator(Methods.OPTIONS);
+export const Trace = MethodDecorator(Methods.TRACE);
+export const Patch = MethodDecorator(Methods.PATCH);
